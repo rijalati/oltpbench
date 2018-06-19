ddc2f170f98caff268a61412288df9d904f56ee8
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2018-06-19 04:55:56 +0000

    refactor recheck script; update compile target

--- ep-recheck.sh
+++ ep-recheck.sh
@@ -2,20 +2,45 @@
 
 typeset -a checks=( $(<./errorprone.hitlist) ) 
 
+(( $# > 0 )) && unset -v checks && typeset -a checks=( ${*} )
 
 print ${checks[@]}
 
+function mkpom
+{
 
-for i in ${!checks[@]}; do
-	print "Creating patch for ${checks[i]}..."
-	mkdir -v "ep/${checks[i]}"
 	print "Generating pom.xml..."
-	sed "s/#|CHECK|#/${checks[$i]}/g" pom_tmpl.xml > pom.xml && wait
+	sed "s/#|CHECK|#/${checks[i]}/g" pom_tmpl.xml > pom.xml && wait
+
+	return
+}
+function mkpdir
+{
+	print "Creating patch dir ${checks[i]}/..."
+	mkdir -v "ep/${checks[i]}"
+
+	return
+}
+function clean
+{
+
 	print "Cleaning project..."
 	{ mvn clean && wait; } > /dev/null 2>&1
-	print "Compiling source to generate patch..."	
+
+	return
+}
+function comp
+{
+
+	print "Compiling source to generate patch..."
 	{ mvn -P fixerrors compile && wait; } > /dev/null 2>&1
-	
+
+	return
+}
+
+function mp
+{
+
 	ls -a
 	if [[ -e error-prone.patch ]]; then
 		print "moving path for ${checks[i]}..."
@@ -24,6 +49,18 @@ for i in ${!checks[@]}; do
 		print "No patch generated, deleting ./ep/${checks[i]} ..."
 		rm -vfr "./ep/${checks[$i]}"
 	fi
+
+	return
+}
+
+for i in ${!checks[@]}; do
+	print "Creating patch for ${checks[i]}..."
+	mkpom
+	mkpdir
+	clean
+	comp
+	mp
+
 done
 
 ls ep
